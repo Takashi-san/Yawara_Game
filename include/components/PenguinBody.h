8eece73f61c08c3e5eb63783e8dda0b89e37dfe0
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2019-05-31 00:34:10 -0300

    Yawara header

--- include/components/PenguinBody.h
+++ include/components/PenguinBody.h
@@ -3,6 +3,7 @@
 #include "Component.h"
 #include "GameObject.h"
 #include "Vec2.h"
+#include "Floor.h"
 
 #include <string>
 #include <iostream>
@@ -10,8 +11,11 @@
 #include <memory>
 
 #define PBODY_ACCEL 500
-#define PBODY_VEL_CAP 300
+#define PBODY_VEL_CAP 150
 #define PBODY_VEL_ANG 2
+
+const int tileSize = 16;
+const float PI = 3.141592;
 
 class PenguinBody : public Component
 {
@@ -21,6 +25,8 @@ private:
 	float linearSpeed;
 	float angle;
 	int hp;
+	Vec2 front;
+	Vec2 back;
 
 public:
 	static PenguinBody *player;
