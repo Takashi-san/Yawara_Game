9f5e8fa63caf3afe054d2719bd0248040e24f0b5
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2019-06-14 16:23:58 -0300

    Add funtions

--- src/type/Rect.cpp
+++ src/type/Rect.cpp
@@ -1,5 +1,17 @@
 #include "Rect.h"
 
+Rect::Rect()
+{
+}
+
+Rect::Rect(float x, float y, float w, float h)
+{
+    this->x = x;
+    this->y = y;
+    this->w = w;
+    this->h = h;
+}
+
 bool Rect::Within(int x, int y) {
 	if ((x >= this->x) && (x <= this->x+w) && (y >= this->y) && (y <= this->y+h)) {
 		return true;
@@ -20,4 +32,51 @@ void Rect::Centered(float ref_x, float ref_y) {
 
 void Rect::Centered(Vec2 ref) {
 	Centered(ref.x, ref.y);
+}
+
+Rect Rect::operator+(const Vec2 &vector)
+{
+
+    return Rect(this->x + vector.x, this->y + vector.y, this->w, this->h);
+}
+
+Rect Rect::operator-(const Vec2 &vector)
+{
+
+    return Rect(this->x - vector.x, this->y - vector.y, this->w, this->h);
+}
+
+Rect Rect::operator*(const Vec2 &vector)
+{
+    return Rect(this->x, this->y, this->w * vector.x, this->h * vector.y);
+}
+
+void Rect::operator+=(const Vec2 &vector)
+{
+
+    this->x += vector.x;
+    this->y += vector.y;
+}
+
+void Rect::operator+=(const Rect &rect)
+{
+
+    this->x += rect.x;
+    this->y += rect.y;
+}
+
+void Rect::operator-=(const Vec2 &vector)
+{
+
+    this->x -= vector.x;
+    this->y -= vector.y;
+}
+
+void Rect::operator=(const Rect &rect)
+{
+
+    this->x = rect.x;
+    this->y = rect.y;
+    this->w = rect.w;
+    this->h = rect.h;
 }
