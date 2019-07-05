8521a501d8d7d98eedae70576c29adab9555cac3
Vieira <vieira.arimatea@gmail.com>
2019-07-04 23:59:42 -0300

    Create the lose end state

--- src/states/EndState.cpp
+++ src/states/EndState.cpp
@@ -6,6 +6,12 @@
 #include "Camera.h"
 #include "InputManager.h"
 
+#define LOSE_END "assets/img/background/game_over.png"
+#define CONT_TXT "assets/img/text/continuar.png"
+#define NEWG_TXT "assets/img/text/novo_jogo.png"
+#define WIN_MUSIC ""
+#define LOSE_MUSIC "assets/audio/musica/tema_triste.ogg"
+
 EndState::EndState() {
 	std::weak_ptr<GameObject> weak_ptr;
 	std::shared_ptr<GameObject> ptr;
@@ -23,7 +29,7 @@ EndState::EndState() {
 		ptr->AddComponent(cmfl);
 
 		// BGM
-		bgMusic.Open("assets/audio/endStateWin.ogg");
+		bgMusic.Open(WIN_MUSIC);
 	
 		// End txt.
 		GameObject *tgo = new GameObject();
@@ -46,27 +52,29 @@ EndState::EndState() {
 		ptr = weak_ptr.lock();
 		ptr->box.x = 0;
 		ptr->box.y = 0;
-		Sprite *sp = new Sprite(*ptr, "assets/img/lose.jpg");
+		Sprite *sp = new Sprite(*ptr, LOSE_END);
 		CameraFollower *cmfl = new CameraFollower(*ptr);
 		ptr->AddComponent(sp);
 		ptr->AddComponent(cmfl);
 
 		// BGM
-		bgMusic.Open("assets/audio/endStateLose.ogg");
+		bgMusic.Open(LOSE_MUSIC);
 	
 		// End txt.
 		GameObject *tgo = new GameObject();
 		weak_ptr = AddObject(tgo);
 		ptr = weak_ptr.lock();
-		Text* tx = new Text(*ptr, "assets/font/Call me maybe.ttf", 50, Text::BLENDED, "Press Space To Play Again", {50, 50, 255, 255});
-		ptr->box.Centered({512, 100});
+		Sprite* tx = new Sprite(*ptr, CONT_TXT);
+		tx->SetScale({0.3,0.3});
+		ptr->box.Centered({280, 500});
 		ptr->AddComponent(tx);
 
 		GameObject *tgo1 = new GameObject();
 		weak_ptr = AddObject(tgo1);
 		ptr = weak_ptr.lock();
-		Text* tx1 = new Text(*ptr, "assets/font/Call me maybe.ttf", 50, Text::BLENDED, "Press ESC To Exit", {255, 50, 50, 255});
-		ptr->box.Centered({512, 175});
+		Sprite* tx1 = new Sprite(*ptr, NEWG_TXT);
+		tx1->SetScale({0.3,0.3});
+		ptr->box.Centered({683, 500});
 		ptr->AddComponent(tx1);
 	}
 
@@ -105,7 +113,7 @@ void EndState::Start(){
 	
 	StartArray();
 
-	bgMusic.Play(-1);
+	bgMusic.Play();
 
 	started = true;
 }
