9a91fee8792004e0d6c02dfb874f1d90084f6fd9
Leonardo <alves.leonardo.t@gmail.com>
2019-07-10 14:46:44 -0300

    Novos sprites
    - Imagens novas
    - Nova animação

--- src/components/DefenseRune.cpp
+++ src/components/DefenseRune.cpp
@@ -4,8 +4,13 @@
 #include "Timer.h"
 
 #define BASE_DEFRUNE_FILE               "assets/img/items/espirito_corrompido.png"
-#define BASE_DEFRUNE_FRAMES             4
-#define BASE_DEFRUNE_FRAMETIME          0.3
+#define BASE_DEFRUNE_FRAMES             9
+#define BASE_DEFRUNE_FRAMETIME          0.15
+
+#define INTERACTED_DEFRUNE_FILE         "assets/img/items/espirito_corrompido_interacao.png"
+#define INTERACTED_DEFRUNE_FRAMES       10
+#define INTERACTED_DEFRUNE_FRAMETIME    0.1
+
 #define ACTIVATED_DEFRUNE_FILE          "assets/img/items/espirito.png"
 #define ACTIVATED_DEFRUNE_FRAMES        8
 #define ACTIVATED_DEFRUNE_FRAMETIME     0.2
@@ -28,23 +33,38 @@ DefenseRune::DefenseRune(GameObject& associated, float defFactor) : Item(associa
 
 void DefenseRune::Update(float dt){
 
-    static Timer cooldownTimer;
-    static bool active = true;
+    static Timer cooldownTimer, changeTimer;
+    static bool active = true, changed = false;
     
     if(Yawara::player){
         float dist = (associated.box.Center() - Yawara::player->GetCenterPos()).Modulo();
 
+        changeTimer.Update(dt);
+
         if(dist <= DEFRUNE_ACTIVATION_DISTANCE && active){
             Yawara::player->Boost(Yawara::DEFBOOST, defFactor);
+            changeTimer.Restart();
+            changed = true;
+            sp->Open(INTERACTED_DEFRUNE_FILE);
+            sp->SetFrameCount(INTERACTED_DEFRUNE_FRAMES);
+            sp->SetFrameTime(INTERACTED_DEFRUNE_FRAMETIME);
+            sp->SetScale(1, 1);
+            associated.box.x -= 16;
+            associated.box.y -= 20;
+            cooldownTimer.Restart();
+            active = false;
+            activationSound->Play(1, MIX_MAX_VOLUME);
+        }
+
+        if((changeTimer.Get() >= INTERACTED_DEFRUNE_FRAMES * INTERACTED_DEFRUNE_FRAMETIME) && changed && !active){
             sp->Open(ACTIVATED_DEFRUNE_FILE);
             sp->SetFrameCount(ACTIVATED_DEFRUNE_FRAMES);
             sp->SetFrameTime(ACTIVATED_DEFRUNE_FRAMETIME);
             sp->SetScale(1, 1);
-            associated.box.x += 32;
-            associated.box.y += 20;
-            cooldownTimer.Restart();
-            active = false;
-            activationSound->Play(1, MIX_MAX_VOLUME);
+            associated.box.x += 48;
+            associated.box.y += 40;
+            changed = false;
+            changeTimer.Restart();
         }
 
         cooldownTimer.Update(dt);
@@ -56,6 +76,7 @@ void DefenseRune::Update(float dt){
             associated.box.x -= 32;
             associated.box.y -= 20;
             active = true;
+            changeTimer.Restart();
         }
     }
 }
