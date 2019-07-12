65bfc90545bd17414ec8464348403271bb0e79c4
Vieira <vieira.arimatea@gmail.com>
2019-07-12 01:35:34 -0300

    Add Shadow

--- include/components/Capelobo.h
+++ include/components/Capelobo.h
@@ -20,15 +20,18 @@ private:
 	
 	float restOffset;
 	float velAttackOffset; // Correct time of attack execution due to some sprites of attack are with 5 sprites and othes 16
+	float soundOffset;
+	std::weak_ptr<GameObject> weak_shadow;
 
 public:
 	static Capelobo *boss;
 
-	Capelobo(GameObject &, float = 0);
+	Capelobo(GameObject &, std::weak_ptr<GameObject>, float = 0);
 	~Capelobo();
 
 	void Start() override;
 	void Update(float) override;
 	void Render() override;
 	bool Is(std::string);
+	void HitSound() override;
 };
