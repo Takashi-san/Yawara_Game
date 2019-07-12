d9b5795bc8a5cbf87bd61f9df486b05432227d90
Vieira <vieira.arimatea@gmail.com>
2019-07-12 05:21:54 -0300

    Minion finished

--- src/components/Dark_Spirit.cpp
+++ src/components/Dark_Spirit.cpp
@@ -11,10 +11,10 @@
 #define SPRT_BULLET_SPEED       750
 #define SPRT_BULLET_DAMAGE      5
 #define SPRT_BULLET_DISTANCE    500
-#define SPRT_BULLET_FRAMES      5
+#define SPRT_BULLET_FRAMES      6
 #define SPRT_BULLET_FM_TIME     0.05
 #define REST_LIMIT              1.5
-const std::string SPRT_BULLET_SOURCE = "assets/img/tapu/disparo.png";
+
 
 #define SPRT_ENEMY_DIST_Y 250
 #define SPRT_ENEMY_DIST_X 250
@@ -27,35 +27,53 @@ const std::string SPRT_BULLET_SOURCE = "assets/img/tapu/disparo.png";
 
 // Movement sprites
 
-const std::string MOVE_RIGHT 	  = "assets/img/dark_spirit/dark_spirit.png";
-const std::string MOVE_RIGHT_DOWN = "assets/img/dark_spirit/dark_spirit.png";
-const std::string MOVE_DOWN		  = "assets/img/dark_spirit/dark_spirit.png";
-const std::string MOVE_LEFT_DOWN  = "assets/img/dark_spirit/dark_spirit.png";
-const std::string MOVE_LEFT		  = "assets/img/dark_spirit/dark_spirit.png";
-const std::string MOVE_LEFT_UP	  = "assets/img/dark_spirit/dark_spirit.png";
-const std::string MOVE_UP		  = "assets/img/dark_spirit/dark_spirit.png";
-const std::string MOVE_RIGHT_UP	  = "assets/img/dark_spirit/dark_spirit.png";
+const std::string MOVE_RIGHT 	  = "assets/img/dark_spirit/Minion roxlo lado direito.png";
+const std::string MOVE_RIGHT_DOWN = "assets/img/dark_spirit/Minion roxlo lado direito.png";
+const std::string MOVE_DOWN		  = "assets/img/dark_spirit/Minion roxo frente.png";
+const std::string MOVE_LEFT_DOWN  = "assets/img/dark_spirit/Minion roxo frente.png";
+const std::string MOVE_LEFT		  = "assets/img/dark_spirit/Minion roxlo lado esquerdo.png";
+const std::string MOVE_LEFT_UP	  = "assets/img/dark_spirit/Minion roxlo lado esquerdo.png";
+const std::string MOVE_UP		  = "assets/img/dark_spirit/Minion roxo costa.png";
+const std::string MOVE_RIGHT_UP	  = "assets/img/dark_spirit/Minion roxo costa.png";
 
 // Sounds
 
 const std::string SPRT_HIT_SOUND	= "assets/audio/sons/spirit/hit.ogg";
 const std::string SPRT_B_ATK_SOUND	= "assets/audio/sons/spirit/ataque.ogg";
-const std::string SPRT_DEATH		= "assets/audio/sons/spirit/grunhido_morte.ogg";
+const std::string SPRT_DEATH		= "assets/audio/sons/spirit/morte.ogg";
+
+// Bullet sprites
+
+const std::string SPRT_BULLET_SOURCE = "assets/img/dark_spirit/efeito_disparo_inimigo.png";
+const std::string SPRT_BULLET_END 	= "assets/img/dark_spirit/disparo_destruido.png";
+
+// Death sprites
+
+const std::string DEATH_RIGHT		= "assets/img/capelobo/capelobo_morte_r.png";
+const std::string DEATH_LEFT	 	= "assets/img/capelobo/capelobo_morte_l.png";
+const std::string EFFECT_1			= "assets/img/capelobo/camada1_efeitomorteboss.png";
+const std::string EFFECT_2			= "assets/img/capelobo/camada2_efeitomorteboss.png";
+const std::string EFFECT_3			= "assets/img/capelobo/camada3_efeitomorteboss.png";
+const std::string EFFECT_4			= "assets/img/capelobo/camada4_efeitomorteboss.png";
+const std::string EFFECT_5			= "assets/img/capelobo/camada5_efeitomorteboss.png";
+
 
 bool sprtStartedMoving = false;
+int sprttimesPlayed = 0;
 
 Dark_Spirit::Dark_Spirit(GameObject & associated) : Enemy(associated){
     moveAllowed = true;
 
 	hp = 70;
 	speed = Vec2{0,0};
-	Sprite *sp = new Sprite(associated, MOVE_DOWN, 4, 0.15);
+	Sprite *sp = new Sprite(associated, MOVE_DOWN, 7, 0.15);
 	Collider *cl = new Collider(associated);
 	associated.AddComponent(sp);
 	associated.AddComponent(cl);
 
     state = MOVING;
     restOffset = 0;
+	alpha = 225;
 }
 
 Dark_Spirit::~Dark_Spirit(){
@@ -65,6 +83,7 @@ Dark_Spirit::~Dark_Spirit(){
 void Dark_Spirit::Update(float dt) {
 
     hitTimer.Update(dt);
+	change_sprite = false;
 
     if (Yawara::player != nullptr)
 	{
@@ -73,7 +92,7 @@ void Dark_Spirit::Update(float dt) {
 		{
 		case MOVING:
 			moveTimer.Update(dt);
-            if(moveTimer.Get() < SPRT_FREE_TIME_MOVEMENT){
+            if(moveTimer.Get() < SPRT_FREE_TIME_MOVEMENT && !((yawaraPos - associated.box.Center()).Modulo() <= SPRT_DIST_DETECT_YAWARA + rangeOffset)){
                 while(speed == Vec2{0,0}){
                     speed = Vec2{rand() % 2 ,rand() % 2 };
                     speed.x *= SPRT_SPEED;
@@ -82,6 +101,40 @@ void Dark_Spirit::Update(float dt) {
                         speed /= 2;
                     Move45(speed); // Correct to move in 45 deg
                 }
+
+				// Set dir to the direction that enemy is facing
+
+				Direction lastDir = dir;
+
+				if (speed.y > 10)
+				{
+					if (speed.x > 10)
+						dir = RIGHT_DOWN;
+					else if (speed.x < -10)
+						dir = LEFT_DOWN;
+					else
+						dir = DOWN;
+				}
+				else if (speed.y < -10)
+				{
+					if (speed.x > 10)
+						dir = RIGHT_UP;
+					else if (speed.x < -10)
+						dir = LEFT_UP;
+					else
+						dir = UP;
+				}
+				else
+				{
+					if (speed.x > 10)
+						dir = RIGHT;
+					else if (speed.x < -10)
+						dir = LEFT;
+				}
+				if (dir != lastDir || sprtStartedMoving){
+					change_sprite = true;
+					sprtStartedMoving = false;
+				}
 
 				moveAllowed = AllowedToMove(speed);
                 
@@ -95,20 +148,18 @@ void Dark_Spirit::Update(float dt) {
 				}
 
             }
-            else
-            {
-                speed = Vec2{0,0};
-                state = RESTING;
-                restTimer.Restart();
-                break;
-            }
-            if((yawaraPos - associated.box.Center()).Modulo() <= SPRT_DIST_DETECT_YAWARA + rangeOffset){
+            else if((yawaraPos - associated.box.Center()).Modulo() <= SPRT_DIST_DETECT_YAWARA + rangeOffset){
                 state = PURSUE;
                 rangeOffset = 0;
                 restOffset = 1;
                 moveTimer.Restart();
                 sprtStartedMoving = true;
-                break;
+            }
+            else
+            {
+                speed = Vec2{0,0};
+                state = RESTING;
+                restTimer.Restart();
             }
             break;
         case PURSUE:
@@ -196,8 +247,7 @@ void Dark_Spirit::Update(float dt) {
 
 		case RESTING:
 			restTimer.Update(dt);
-			if (!change_sprite)
-				change_sprite = true;
+			change_sprite = false;
 
 			// Rest for a determinated time
 			if (restTimer.Get() > REST_LIMIT - restOffset)
@@ -219,7 +269,7 @@ void Dark_Spirit::Update(float dt) {
 
 		case BASIC_ATTACK:
             attackTimer.Update(dt);
-            if (attackTimer.Get() > 0.5)
+            if (attackTimer.Get() > 1.5)
 			{
                 GameObject *bullet_GO = new GameObject;
                 std::weak_ptr<GameObject> weak_bullet = Game::GetInstance().GetCurrentState().AddObject(bullet_GO);
@@ -230,6 +280,8 @@ void Dark_Spirit::Update(float dt) {
 				if (angle <= 0)
 					angle += 360;
                     
+				Direction lastDir = dir;
+
                 if(angle >= 337.5 || angle < 22.5){
 					angle = 360;
 					dir = RIGHT;
@@ -263,9 +315,12 @@ void Dark_Spirit::Update(float dt) {
 					dir = RIGHT_UP;
 				}
 
+				if (dir != lastDir || sprtStartedMoving)
+					change_sprite = true;
+
                 angle *= PI/180;
 
-                Bullet *bullet = new Bullet(*ptr, angle, SPRT_BULLET_SPEED, SPRT_BULLET_DAMAGE, SPRT_BULLET_DISTANCE, SPRT_BULLET_SOURCE, SPRT_BULLET_FRAMES, SPRT_BULLET_FM_TIME, true);
+                Bullet *bullet = new Bullet(*ptr, angle, SPRT_BULLET_SPEED, SPRT_BULLET_DAMAGE, SPRT_BULLET_DISTANCE, SPRT_BULLET_SOURCE, SPRT_BULLET_END, SPRT_BULLET_FRAMES, SPRT_BULLET_FM_TIME, true);
                 ptr->box.Centered(associated.box.Center());
                 Vec2 offset(associated.box.w / 2, 0);
                 offset.Rotate(angle);
@@ -278,45 +333,91 @@ void Dark_Spirit::Update(float dt) {
             }
 			break;
         default:
+			state = RESTING;
+			restTimer.Restart();
             break;
 		}
         if(hp <= 0){
             state = RESTING;
             deathTimer.Update(dt);
             
-            GameObject *go = new GameObject();
-            std::weak_ptr<GameObject> weak_ptr = Game::GetInstance().GetCurrentState().AddObject(go);
-            std::shared_ptr<GameObject> ptr = weak_ptr.lock();
-            Sprite *sp;
 
-            if(deathTimer.Get() < DEATH_EFFECT_TIME){}
-            else{
-                if(Capelobo::boss != nullptr){
-                    GameObject *goSpirit = new GameObject();
-                    std::weak_ptr<GameObject> weak_ptr_sprt = Game::GetInstance().GetCurrentState().AddObject(goSpirit);
-                    std::shared_ptr<GameObject> ptr_sprt = weak_ptr_sprt.lock();
-                    Dark_Spirit *sprt = new Dark_Spirit(*ptr_sprt);
-                    ptr_sprt->box.Centered({associated.box.x + rand()%2500 - rand()%2500, associated.box.y + rand()%500 - rand()%500});
-                    if(ptr_sprt->box.Center().x < 0)
-                        ptr_sprt->box.Centered({associated.box.x + rand()%2500, ptr_sprt->box.Center().y});
-                    if(ptr_sprt->box.Center().y < 0)
-                        ptr_sprt->box.Centered({ptr_sprt->box.Center().x, associated.box.y + rand()%2500});
-                    ptr_sprt->AddComponent(sprt);
-                }
-                associated.RequestDelete();
-				// sp = new Sprite(*ptr, DEATH_RIGHT, 9, 0.1, 9 * 0.1);
-                // Sound *so = new Sound(*ptr, "assets/audio/boom.wav");
-                // ptr->AddComponent(so);
+			GameObject *go = new GameObject();
+            std::weak_ptr<GameObject> weak_sound = Game::GetInstance().GetCurrentState().AddObject(go);
+            std::shared_ptr<GameObject> ptr_sound = weak_sound.lock();
+			Sound *so = new Sound(*ptr_sound, SPRT_DEATH);
+			Sprite *sp = static_cast<Sprite *>(associated.GetComponent("Sprite"));
+			ptr_sound->AddComponent(so);
 
-                // so->Play(1);
-            }
+			if(sprttimesPlayed == 0){
+				so->Play(1, MIX_MAX_VOLUME);
+				++sprttimesPlayed;
+			}
+			if(deathTimer.Get() < DEATH_EFFECT_TIME){
+				if(alpha > 0)
+					alpha -= dt;
+				sp->SetAlphaMod(alpha);
+			}
+
+			associated.RequestDelete();
         }
 	}
 }
 
 void Dark_Spirit::Render() {
     Vec2 position = associated.box.Center();
+	Sprite *sp = static_cast<Sprite *>(associated.GetComponent("Sprite"));
+	if (change_sprite && sp)
+	{
+		change_sprite = false;
+		sp->SetFrameTime(0.15);
+		switch (dir)
+		{
+		case RIGHT:
+			sp->Open(MOVE_RIGHT);
+			sp->SetFrameCount(7);
+			break;
 
+		case LEFT:
+			sp->Open(MOVE_LEFT);
+			sp->SetFrameCount(7);
+			break;
+
+		case UP:
+			sp->Open(MOVE_UP);
+			sp->SetFrameCount(7);
+			break;
+
+		case DOWN:
+			sp->Open(MOVE_DOWN);
+			sp->SetFrameCount(7);
+			break;
+
+		case RIGHT_UP:
+			sp->Open(MOVE_RIGHT_UP);
+			sp->SetFrameCount(7);
+			break;
+
+		case RIGHT_DOWN:
+			sp->Open(MOVE_RIGHT_DOWN);
+			sp->SetFrameCount(7);
+			break;
+
+		case LEFT_UP:
+			sp->Open(MOVE_LEFT_UP);
+			sp->SetFrameCount(7);
+			break;
+
+		case LEFT_DOWN:
+			sp->Open(MOVE_LEFT_DOWN);
+			sp->SetFrameCount(7);
+			break;
+
+		default:
+			break;
+		}
+	}
+	associated.box.Centered(position);
 }
 
 bool Dark_Spirit::Is(std::string type){
