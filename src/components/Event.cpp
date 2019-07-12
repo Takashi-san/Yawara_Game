e8266db026288d117e9553dc4b24a046afde2e47
Vieira <vieira.arimatea@gmail.com>
2019-07-12 11:07:42 -0300

    Prevents segfault

--- src/components/Event.cpp
+++ src/components/Event.cpp
@@ -13,21 +13,22 @@ Event::Event(GameObject& associated, float x, float y, float w, float h) : Compo
 }
 
 void Event::Update(float dt) {
+    if(Yawara::player){
+        if(associated.box.Within(Yawara::player->GetCenterPos().x, Yawara::player->GetCenterPos().y)){
+            GameObject* enemygo = new GameObject();
+            std::weak_ptr<GameObject> weak_ptr;
+            std::shared_ptr<GameObject> ptr;
+            weak_ptr = Game::GetInstance().GetCurrentState().AddObject(enemygo);
+            ptr = weak_ptr.lock();
 
-    if(associated.box.Within(Yawara::player->GetCenterPos().x, Yawara::player->GetCenterPos().y)){
-        GameObject* enemygo = new GameObject();
-        std::weak_ptr<GameObject> weak_ptr;
-	    std::shared_ptr<GameObject> ptr;
-        weak_ptr = Game::GetInstance().GetCurrentState().AddObject(enemygo);
-        ptr = weak_ptr.lock();
+            Dark_Spirit* dark = new Dark_Spirit(*ptr);
+            ptr->AddComponent(dark);
 
-        Dark_Spirit* dark = new Dark_Spirit(*ptr);
-        ptr->AddComponent(dark);
-
-        ptr->box.x = associated.box.x + rand()%((int) associated.box.w);
-        ptr->box.y = associated.box.y + rand()%((int) associated.box.h);
-        
-        associated.RequestDelete();
+            ptr->box.x = associated.box.x + rand()%((int) associated.box.w);
+            ptr->box.y = associated.box.y + rand()%((int) associated.box.h);
+            
+            associated.RequestDelete();
+        }
     }
 }
 
