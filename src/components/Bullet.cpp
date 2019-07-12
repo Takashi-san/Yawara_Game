c750bea0d1077e5b78508bfa8b9cf78fc5cba576
Vieira <vieira.arimatea@gmail.com>
2019-07-12 05:25:39 -0300

    Addapt bullet

--- src/components/Bullet.cpp
+++ src/components/Bullet.cpp
@@ -1,9 +1,8 @@
 #include "Bullet.h"
 #include "Sprite.h"
 #include "Collider.h"
-#include <algorithm>
 
-Bullet::Bullet(GameObject& associated, float angle, float speed, int damage, float maxDistance, std::string sprite, int frameCount, float frameTime, bool targetsPlayer, bool hitDie) : Hitbox(associated, damage, targetsPlayer, maxDistance/speed, hitDie) {
+Bullet::Bullet(GameObject& associated, float angle, float speed, int damage, float maxDistance, std::string sprite, std::string spriteEnd, int frameCount, float frameTime, bool targetsPlayer) : Component(associated) {
 	Sprite* sp = new Sprite(associated, sprite, frameCount, frameTime);
 	sp->SetScale(1, 1);
 	Collider *cl = new Collider(associated);
@@ -16,15 +15,17 @@ Bullet::Bullet(GameObject& associated, float angle, float speed, int damage, flo
 	this->speed.Rotate(angle);
 	associated.angleDeg = angle * (180/PI);
 	this->damage = damage;
-	this->timeLeft = maxDistance/speed;
+	this->distanceLeft = maxDistance;
+	this->spriteEnd = spriteEnd;
 }
 
 void Bullet::Update(float dt) {
 	associated.box.x += speed.x*dt;
 	associated.box.y += speed.y*dt;
-	timeLeft -= dt;
-	if(timeLeft <= 0)
+	distanceLeft -= speed.Modulo()*dt;
+	if (distanceLeft <= 0) {
 		associated.RequestDelete();
+	}
 }
 
 void Bullet::Render() {
@@ -32,7 +33,7 @@ void Bullet::Render() {
 }
 
 bool Bullet::Is(std::string type) {
-	return !std::min(strcmp(type.c_str(), "Bullet"), strcmp(type.c_str(), "Hitbox"));
+	return !strcmp(type.c_str(), "Bullet");
 }
 
 int Bullet::GetDamage() {
@@ -40,24 +41,10 @@ int Bullet::GetDamage() {
 }
 
 void Bullet::NotifyCollision(GameObject& other) {
-	if (other.GetComponent("Enemy") && !targetsPlayer) {
+	if ((other.GetComponent("Enemy") && !targetsPlayer) || (other.GetComponent("Yawara") && targetsPlayer)) {
 		Vec2 pos = associated.box.Center();
 		Sprite *sp = static_cast<Sprite *>(associated.GetComponent("Sprite"));
-		sp->Open("assets/img/tapu/disparo_colisão.png");
-		sp->SetFrameCount(6);
-		sp->SetFrameTime(0.05);
-		sp->SetSelfDestruct(6*0.05);
-		speed.x = speed.x*0.3;
-		speed.y = speed.y*0.3;
-		associated.box.Centered(pos);
-
-		Collider *cl = static_cast<Collider *>(associated.GetComponent("Collider"));
-		associated.RemoveComponent(cl);
-	}
-	if (other.GetComponent("Yawara") && targetsPlayer) {
-		Vec2 pos = associated.box.Center();
-		Sprite *sp = static_cast<Sprite *>(associated.GetComponent("Sprite"));
-		sp->Open("assets/img/tapu/disparo_colisão.png");
+		sp->Open(spriteEnd);
 		sp->SetFrameCount(6);
 		sp->SetFrameTime(0.05);
 		sp->SetSelfDestruct(6*0.05);
